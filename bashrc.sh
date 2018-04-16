cf602b29e953ca1d8331299b9954472aabc26293
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2018-04-16 17:35:51 +0200

    Added figlet alias

--- bashrc.sh
+++ bashrc.sh
@@ -20,6 +20,21 @@ alias dps='docker ps'
 alias dl='docker logs'
 alias dk='docker kill'
 alias dprune='docker system prune --all --force'
+
 dbuild() {
     docker build -t mathieubrun/${PWD##*/}:latest .
+}
+
+jekyll() {
+    docker run --rm -ti \
+        --workdir '/code' \
+        -v "$(pwd):/code" \
+        -v "$(pwd)/.gems:/usr/local/bundle" \
+        -p "4000:4000" \
+        mathieubrun/jekyll:latest
+}
+
+figlet() {
+    docker run --rm -ti \
+        mathieubrun/figlet:latest "$@"
 }
