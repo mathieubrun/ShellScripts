d60373acfa9dff88679c7c73490decfbbb3d1616
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2018-04-16 16:17:11 +0200

    Added docker aliases

---
+++ bashrc_docker.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+jekyll() {
+    docker run -ti \
+        --workdir '/code' \
+        -v "$(pwd):/code" \
+        -v "$(pwd)/.gems:/usr/local/bundle" \
+        -p "4000:4000" \
+        mathieubrun/jekyll:latest "$@"
+}
