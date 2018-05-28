b7c846a6c7976ea6f1f889a15fd0cd1bcb7436ff
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2018-05-28 21:53:32 +0200

    Removed export of MSYS_NO_PATHCONV

--- bash_profile.sh
+++ bash_profile.sh
@@ -4,12 +4,6 @@ export SHELL_SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 alias ls='LC_COLLATE=C ls --group-directories-first --color'
 
-# windows specific
-if [[ "$OSTYPE" == "msys"* ]]; then
-    # https://github.com/docker/toolbox/issues/371
-    export MSYS_NO_PATHCONV=1
-fi
-
 # mac os specific
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
